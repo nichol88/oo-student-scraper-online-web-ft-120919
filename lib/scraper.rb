dfb36eaa7288fcde41e6a50813f6c05334761d97
Brian Nicholls <nichol88@gmail.com>
2020-01-06 14:53:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML.parse(open(index_url))
+    result = [{},{},{}]
   end
 
   def self.scrape_profile_page(profile_url)
