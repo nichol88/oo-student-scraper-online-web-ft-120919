24590d8ac0047128f8cfef4735c8ba32d04c0c5d
Brian Nicholls <nichol88@gmail.com>
2020-01-06 15:18:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,13 +6,13 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML.parse(open(index_url))
     students = doc.css(".student-card")
-    result = students.map{ |s|
+    students.each{ |s|
       s[:name] = s[:id].gsub("-", " ").split("card")[0].strip
       s[:location] = s.children[1].css("p")[0].children.text
       s[:profile_url] = s.css("a")[0]["href"]
     }
     result
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
