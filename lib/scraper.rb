3b1eb995cc6ac15cd7cde686d160f9eddc218006
Brian Nicholls <nichol88@gmail.com>
2020-01-06 15:25:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
     result = []
     students.each{ |s|
       f = {}
-      f[:name] = s[:id].gsub("-", " ").split("card")[0].strip
+      f[:name] = s.children[1].css(".student-name").text
       f[:location] = s.children[1].css("p")[0].children.text
       f[:profile_url] = s.css("a")[0]["href"]
       result.push(f)
