db6717c4989d885419af3529a70e3704cd7c204c
Brian Nicholls <nichol88@gmail.com>
2020-01-06 15:02:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,10 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML.parse(open(index_url))
     students = doc.css(".student-card")
+    students.map{ |s|
+
+    }
+
     binding.pry
   end
 
